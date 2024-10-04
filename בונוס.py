0221e912db98e59c30b6528e4628bebf21f9fd75
Altman156272829 <noamaltnan@gmail.com>
2024-10-04 22:42:17 +0300

    לבד

--- בונוס.py
+++ בונוס.py
@@ -1,18 +1,37 @@
 from PIL import Image
+import io
+code =int(input("enter your code"))
+def to_binary(data):
+    return ''.join(format(byte, '08b') for byte in data)
 
-def image_to_binary(image_path):
+def xor_encrypt(data, key):
+    return ''.join(chr(ord(c) ^ key) for c in data)
+
+def main():
     try:
-        with Image.open(image_path) as img:
-            binary_data = []
-            for pixel in img.getdata():
-                r, g, b = pixel[:3]  # וודא שאתה מתייחס רק לרכיבי ה-RGB של הפיקסל
-                binary_pixel = f'{r:08b} {g:08b} {b:08b}'
-                binary_data.append(binary_pixel)
-                print(binary_pixel)  # הדפסת נתוני הפיקסלים הבינאריים
-            return binary_data
+        
+        with open(r"C:\Users\User\Downloads\אקר.jpeg", 'rb') as image:
+            binary_data = to_binary(image.read()) # החלק שבו יש בינארי
+            key = 123  # מפתח ההצפנה
+            encrypted_data = xor_encrypt(binary_data, key)
+            with open(r'C:\\Users\\User\\Downloads\\image_encrypted.bin', 'wb') as encrypted_file:
+                encrypted_file.write(bytes(encrypted_data, 'utf-8'))
+            print("Data encrypted and saved successfully.")
     except Exception as e:
-        print(f"Error: {e}")     
+        print(f"Error: {e}")
 
-image_path = r"C:\Users\User\Downloads\אקר.jpeg"  # ודא שהמסלול נכון
-image_to_binary(image_path)
+def decoder():
+    with open(r'C:\\Users\\User\\Downloads\\image_encrypted.bin','rb') as text:
+        encrypted_file = text.read()
+        bin_encrypted_file = int(to_binary(encrypted_file))
+        print(bin_encrypted_file)
+        data = bin_encrypted_file ^ code
+        data = bytes(data)
+    with open(r'C:\\Users\\User\\Downloads\\secret.bin', 'wb') as secret:
+        secret.write(data)
+        Img = Image.open(secret)
+        Img.show()
 
+main()
+
+decoder()
