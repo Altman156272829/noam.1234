34e7f547c470858b0d9411f158f00e8edfd23140
Altman156272829 <noamaltnan@gmail.com>
2024-10-04 11:25:46 +0300

    נסיון

--- בונוס.py
+++ בונוס.py
@@ -1,5 +1,19 @@
+from PIL import Image
+import io
 
+# טען את התמונה
+from PIL import Image
 
-with open(r"C:\Users\User\Downloads\hacker.htm","rb") as file:
-    a= file.read()
-print(a)
+with open(r'C:\Users\User\Downloads\אקר.jpeg', 'rb') as file:
+    img = Image.open(file)
+    img.show()
+# שמור את התמונה כייצוג בינארי
+with io.BytesIO() as output:
+    img.save(output, format="JPEG")
+    binary_data = output.getvalue()
+
+# הדפס את המידע הבינארי או שמור לקובץ
+print(binary_data)
+with open('output_image.bin', 'wb') as f:
+    f.write(binary_data)
+
